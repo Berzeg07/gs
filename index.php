a2161c66de173e322cca864352d0d2502dbf7154
Berzeg07 <berezg86@gmail.com>
2017-09-03 23:23:53 +0300

    3.09

---
+++ index.php
@@ -0,0 +1,274 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>Главная</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/fonts.css">
+
+	
+</head>
+<body>
+
+	<!-- header -->
+	<div class="header">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<div class="header-left">
+						<div class="header-left_item">
+							<img class="img-responsive" src="img/logotip.png" alt="">
+						</div>
+						<div class="header-left_item">
+							<span>8 800 550 98 83</span><br/>
+							<span>8 928 717 88 83</span><br/>
+							<span>КБР, г.Нальчик, ул.Балкарская 11</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<div class="nav">
+						<ul>
+							<li><a href="#">о проекте</a></li>
+							<li><a href="#">генплан</a></li>
+							<li><a href="#">квартиры</a></li>
+							<li><a href="#">акции</a></li>
+							<li><a href="#">контакты</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="clear"></div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- box-1 -->
+	<div class="box-1">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<div class="box-1_txt">
+						<p>
+							Квартиры от 4,4 млн руб.
+							Площади от 40 до 91 кв. м
+							корпуса «Альфа» и «Бета» – сданы досрочно
+							корпус «Гамма» – 3 кв. 2018 г.
+						</p>
+						<p class="p-next">
+							«Альфа Центавра» - это жилой комплекс комфорт-класса, состоящий из четырех двухсекционных 17-этажных домов, расположенный в г. Химки на ул. 9 Мая всего в 2 км от Москвы и рядом с излюбленным местом отдыха горожан - парком «Дубки».
+						</p>
+						<span class="phone">8 800 550 98 83</span>
+						<span class="kr"><b>круглосуточно</b></span>
+					</div>
+
+					<div class="box-1_item">
+
+						<div class="box-1_item__block">
+							<div class="box-1_img">	
+								<img src="img/it1.png" alt="">
+							</div>
+							<span>Наши предложения</span>
+						</div>
+
+						<div class="box-1_item__block">
+							<div class="box-1_img">	
+								<img src="img/it2.png" alt="">
+							</div>
+							<span>Наши проекты</span>
+						</div>
+
+						<div class="box-1_item__block">
+							<div class="box-1_img">	
+								<img src="img/it3.png" alt="">
+							</div>
+							<span>Предложения от <br/>наших партнеров</span>
+						</div>
+
+						<div class="box-1_item__block">
+							<div class="box-1_img">	
+								<img src="img/it4.png" alt="">
+							</div>
+							<span>Вторичное жилье</span>
+						</div>
+
+						<div class="box-1_item__block">
+							<div class="box-1_img">	
+								<img src="img/it5.png" alt="">
+							</div>
+							<span>Коммерческая <br/>недвижимость продажа</span>
+						</div>
+						<div class="clear"></div>
+
+					</div>
+				</div>
+
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 hidden-xs hidden-sm"></div>
+				<div class="clear"></div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- box-2 -->
+	<div class="box-2">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 hidden-xs hidden-sm"></div>
+
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<div class="box-2_txt">
+						<h2>О проекте</h2>
+						<p>
+							Жилой комплекс «Альфа Центавра» новая звезда в самом центре Химок! Космическая тематика ЖК поддерживается архитектурной составляющей в виде домов, фасады которых напоминают ракеты. По плану застройки планируется возведение четырех двухсекционных монолитных домов высотой 17 этажей, два из которых, корпуса «Альфа» и «Бета», уже возведены и сданы досрочно на радость новым жителям комплекса. В домах представлены 1-, 2- и 3-комнатные квартиры площадью от 40 до 91 м2 и высотой потолков 2.8 м. На верхних этажах жильцам предлагаются видовые квартиры. Будущие покупатели могут выбрать между свободной планировкой или квартирой с отделкой.
+						</p>
+					</div>
+					<div id="slider">
+
+						<div class="item">
+							<div class="item__box">
+								<img src="img/sl1.png" alt="">
+							</div>
+						</div>	
+
+						<div class="item">
+							<div class="item__box">
+								<img src="img/sl2.jpg" alt="">
+							</div>
+						</div>	
+
+						<div class="item">
+							<div class="item__box">
+								<img src="img/sl1.png" alt="">
+							</div>
+						</div>	
+
+						<div class="item">
+							<div class="item__box">
+								<img src="img/sl2.jpg" alt="">
+							</div>
+						</div>	
+
+					</div>
+				</div>
+				<div class="clear"></div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- box-3 -->
+	<div class="box-3">
+
+		<div class="container">
+			<h2>корпуса на генплане</h2>
+			<div class="plan">
+				<img class="img-responsive" src="img/plan.jpg" alt="">
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></div>
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<table>
+						<td></td>
+					</table>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></div>
+				<div class="clear"></div>
+			</div>
+		</div>
+
+	</div>
+
+	<!-- box-4 -->
+	<div class="box-4">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<div class="box-4_txt">
+						<h3>Акции*</h3>
+						<p><span>Рассрочка 0%.</span> Условия и сроки уточняйте у менеджеров.</p>
+						<p><span>Кухня в подарок</span> при покупке квартиры в корпусе «Гамма»!</p>
+						<p><span>Бесплатная регистрация</span> права собственности в корпусах «Альфа» и «Бета».</p>
+						<p><span>Квартиры с отделкой</span> от застройщика в корпусе «Гамма»!</p>
+						<p>* Условия и сроки действия акции уточняйте у ваших менеджеров или по телефону +7 (495) 236-84-70</p>
+					</div>
+				</div>
+
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<div class="box-4_txt">
+						<h3>Ипотека</h3>
+						<p><span>Рассрочка 0%.</span> Условия и сроки уточняйте у менеджеров.</p>
+						<p><span>Кухня в подарок</span> при покупке квартиры в корпусе «Гамма»!</p>
+						<p><span>Бесплатная регистрация</span> права собственности в корпусах «Альфа» и «Бета».</p>
+						<p><span>Квартиры с отделкой</span> от застройщика в корпусе «Гамма»!</p>
+
+						<div class="box-4_img">
+							<div class="box-4_img__item">
+								<img src="img/vtb.png" alt="">
+							</div>
+							<div class="box-4_img__item">
+								<img src="img/sber.png" alt="">
+							</div>
+						</div>
+
+					</div>
+				</div>
+				<div class="clear"></div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- box-5 -->
+	<div class="box-5">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<div class="box-5_form">
+
+						<form action="#">
+							<span class="sp1">Узнайте стоимость разработки <br/>
+								под ваши задачи
+							</span><br/>
+							<span class="sp2">Отправьте заявку прямо сейчас</span>
+							<input type="text" placeholder="Ваше имя *" required>
+							<input type="text" class="phone" placeholder="Телефон *" required>
+							<button>Отправить</button>
+						</form>
+
+					</div>
+				</div>
+
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2895.1418513138087!2d43.58926831506772!3d43.47850997912773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x405a06a2cf32afbb%3A0xd5d79f660de7c909!2sul.+Balkarskaya%2C+11%2C+Nalchik%2C+Kabardino-Balkarskaya+Republits%2C+360017!5e0!3m2!1sen!2sru!4v1504467534996" width="100%" height="520" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+				<div class="clear"></div>
+
+			</div>
+		</div>
+	</div>
+
+
+	<!-- JS_BLOCK -->
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/fonts.js"></script>
+	<script src="js/common.js"></script>
+
+
+
+
+</body>
+</html>
---
+++ js/common.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	// Мобильное меню 
+	$(".fa-bars").click(function(m){
+		m.preventDefault();
+		$(".header__menu-box").slideToggle(500);
+	});
+	// Конец Мобильное меню
+
+    $(".phone").mask("+ 7 (999) 999 - 99 - 99?"); 
+
+    var owl2 = $("#slider");
+    owl2.owlCarousel({
+        loop:true,
+        nav:true, 
+        autoplay:false,
+        smartSpeed:1000,
+        margin:15,
+        center:false,     //если нужны обрезаные края
+        navText:['<span class="nav-left"></span>','<span class="nav-right"></span>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2 
+            },
+            768:{
+                items:2
+            },      
+        }
+    });
+
+});//END READY
