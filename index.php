d5412aa1c0d2462f10082e97813391047ef07ced
Berzeg07 <berezg86@gmail.com>
2017-09-04 14:01:10 +0400

    4.09

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
 	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
 	<link rel="stylesheet" href="css/style.css">
+	
 	<link rel="stylesheet" href="css/fonts.css">
 
 	
